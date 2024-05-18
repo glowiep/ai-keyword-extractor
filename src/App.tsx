285be7a23e4d8875eb74555b4071d51ffe3a6b69
Gloria Lim <glorialimartt@gmail.com>
2024-05-18 18:20:57 -0400

    Add Header component to App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import { Container, Box, Text } from "@chakra-ui/react"
+import Header from "./components/Header";
 
 function App() {
   
   return (
     <Box bg='gray.800' color='white' height='100vh' paddingTop={130} >
       <Container maxW='3xl' centerContent>
-        <Text>AI Keyword Extractor</Text>
+        <Header />
 
       </Container>
     </Box>
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,14 +1,16 @@
-import { Heading, Image, Text } from "@chakra-ui/react";
+import { Heading, Text } from "@chakra-ui/react";
 
 function Header() {
-  <>
-    <Heading color="white" marginBottom="1rem">
-      AI Keyword Extractor
-    </Heading>
-    <Text fontSize={25} textAlign="center">
-      Enter a text snippet below and we will extract the keywords for you.
-    </Text>
-  </>
+  return (
+    <>
+      <Heading color="white" marginBottom="1rem">
+        AI Keyword Extractor
+      </Heading>
+      <Text fontSize='normal' textAlign="center">
+        Enter a text snippet below and we will extract the keywords for you.
+      </Text>
+    </>
+  )
 };
 
 export default Header;
