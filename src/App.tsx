f826fc129b261f30cd344dbfa61169e5940217ea
Gloria Lim <glorialimartt@gmail.com>
2024-05-18 18:49:29 -0400

    Implement framer motion div

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,6 @@
-import { Container, Box, Text } from "@chakra-ui/react"
+import { Container, Box } from "@chakra-ui/react"
+import { motion } from "framer-motion";
+
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 
@@ -7,8 +9,28 @@ function App() {
   return (
     <Box bg='gray.800' color='white' height='100vh' paddingTop={130} >
       <Container maxW='3xl' centerContent>
-        <Header />
-        <Footer />
+        <motion.div
+          initial={{ opacity: 0, y: 100 }}
+          animate={{ opacity: 1, y: 0 }}
+          transition={{ delay: 0.5 }}
+          style={{
+            textAlign: 'center',
+            paddingTop: "2.25em",
+          }}
+        >
+          <Header />
+        </motion.div>
+        <motion.div
+          initial={{ opacity: 0, y: 100 }}
+          animate={{ opacity: 1, y: 0 }}
+          transition={{ delay: 1 }}
+          style={{
+            textAlign: 'center',
+            paddingTop: "2.25em",
+          }}
+        >
+          <Footer />
+        </motion.div>
       </Container>
     </Box>
   );
