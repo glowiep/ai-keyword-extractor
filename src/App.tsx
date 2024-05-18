107102391630ff9e24e66625ae6c51d7409ec9b3
Gloria Lim <glorialimartt@gmail.com>
2024-05-18 18:31:08 -0400

    Initialize Footer

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { Container, Box, Text } from "@chakra-ui/react"
 import Header from "./components/Header";
+import Footer from "./components/Footer";
 
 function App() {
   
@@ -7,7 +8,7 @@ function App() {
     <Box bg='gray.800' color='white' height='100vh' paddingTop={130} >
       <Container maxW='3xl' centerContent>
         <Header />
-
+        <Footer />
       </Container>
     </Box>
   );
--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,7 +1,14 @@
-
+import { Box, Flex, Text } from "@chakra-ui/react";
 
 function Footer() {
-  
+  return (
+    <Box marginTop={50}>
+      <Flex justifyContent='center' alignItems='center'>
+        {/* <Image src={logo-url} /> */}
+        <Text>Powered By Open AI</Text>
+      </Flex>
+    </Box>
+  )
 };
 
 export default Footer;
