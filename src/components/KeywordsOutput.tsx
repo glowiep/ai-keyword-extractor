f30a80b04c841f6c6d3051aee622631efbc98e62
Gloria Lim <glorialimartt@gmail.com>
2024-05-19 10:23:06 -0400

    Add comments

--- src/components/KeywordsOutput.tsx
+++ src/components/KeywordsOutput.tsx
@@ -12,15 +12,29 @@ function KeywordsOutput() {
       {showKeywords && (isLoading ? (
         <CircularProgress isIndeterminate color="blue.300" />
       ) : (
-        <Box bg="blue.700" w={300} padding="1em" borderRadius="1em" maxH={300}>          
-          <Heading color="white" marginBottom="1rem" size="normal">
-            Keywords Output ({keywords?.length}):
-          </Heading>
-          <Text fontSize="normal" textAlign="left" paddingLeft="1em">
-            {/* ARRAY OF KEYWORDS HERE */}
-            {keywords?.join(", ") }
-          </Text>
-        </Box>
+        <>
+          <Box bg="blue.700" w={300} padding="1em" borderRadius="1em" maxH={300}>          
+            <Heading color="white" marginBottom="1rem" size="normal">
+              Keywords Output ({keywords?.length}):
+            </Heading>
+            <Text fontSize="normal" textAlign="left" paddingLeft="1em">
+              {/* ARRAY OF KEYWORDS HERE */}
+              {keywords?.join(", ") }
+            </Text>
+          </Box>
+          {/* Copy icon here */}
+          {/* Example:
+          
+            <ContentCopyIcon
+            onClick={() => copyMemo(props.id)} 
+            fontSize="small"
+            sx={{
+              ":hover": {
+                cursor: "pointer",
+              },
+            }}
+          /> */}
+        </>
       ))}
     </>
   );
