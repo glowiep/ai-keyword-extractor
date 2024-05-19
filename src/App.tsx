dfdf3dc9f42d5e7fae4bd6988dee44d1d40ae0d7
Gloria Lim <glorialimartt@gmail.com>
2024-05-19 10:15:27 -0400

    Add KeywordsOutput component with logic to show when certain conditions are met

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,14 @@
-import { Container, Box } from "@chakra-ui/react"
+import { Flex, Container, Box } from "@chakra-ui/react"
 import { motion } from "framer-motion";
 
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import TextInput from "./components/TextInput";
+import KeywordsOutput from "./components/KeywordsOutput";
 
 function App() {
   return (
-    <Box bg='gray.800' color='white' height='100vh' paddingTop={130} >
+    <Flex bg='gray.800' color='white' height='100vh' justifyContent="center" alignItems="center">
       <Container maxW='3xl' centerContent>
         <motion.div
           initial={{ opacity: 0, y: 100 }}
@@ -40,10 +41,11 @@ function App() {
             paddingTop: "2.25em",            
           }}
         >
+          <KeywordsOutput />
           <Footer />
         </motion.div>
       </Container>
-    </Box>
+    </Flex>
   );
 };
 
--- src/components/KeywordsOutput.tsx
+++ src/components/KeywordsOutput.tsx
@@ -1,7 +1,29 @@
-
+import { Heading, Text, CircularProgress, Box } from "@chakra-ui/react";
+import { useAppContext } from "../contexts/AppContext";
 
 function KeywordsOutput() {
-  
-};
+  // const { state } = useAppContext();
+  // const { isLoading, showKeywords } = state;
+  const showKeywords = true;
+  const isLoading = false;
+  const keywords = ["test", "q", "1","test", "q", "1","test", "q", "1","test", "q", "1","test", "q", "1","test", "q", "1",]
+  return (
+    <>
+      {showKeywords && (isLoading ? (
+        <CircularProgress isIndeterminate color="blue.300" />
+      ) : (
+        <Box bg="blue.700" w={300} padding="1em" borderRadius="1em" maxH={300}>          
+          <Heading color="white" marginBottom="1rem" size="normal">
+            Keywords Output ({keywords?.length}):
+          </Heading>
+          <Text fontSize="normal" textAlign="left" paddingLeft="1em">
+            {/* ARRAY OF KEYWORDS HERE */}
+            {keywords?.join(", ") }
+          </Text>
+        </Box>
+      ))}
+    </>
+  );
+}
 
 export default KeywordsOutput;
