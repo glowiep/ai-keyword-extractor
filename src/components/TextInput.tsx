131047b38de0b61159193354b60080588d33f376
Gloria Lim <glorialimartt@gmail.com>
2024-05-18 20:09:58 -0400

    Create TextInput

--- src/components/TextInput.tsx
+++ src/components/TextInput.tsx
@@ -1,7 +1,57 @@
-
+import { useState } from "react";
+import { Textarea } from "@chakra-ui/react";
+import { useToast } from "@chakra-ui/react";
+import { MotionButton } from "../utils/motionUtils";
 
 function TextInput() {
-  
-};
+  const [input, setInput] = useState("");
+  const toast = useToast();
+
+  const submitText = function () {
+    if (input === '') {
+      toast({
+        title: 'Text field is empty.',
+        description: 'Please enter some text before submitting.',
+        status: 'error',
+        duration: 5000,
+        isClosable: true,
+        position: "top"
+      });
+      return;
+    }
+    extractKeywords(input);
+  };
+
+  const submitButtonStyles = {
+    whileTap: { scale: 0.5, boxShadow: "0px 0px 10px rgba(0, 0, 0, 0.2)" },
+    whileHover: { scale: 1.1, color: "blue" },
+    variant: "solid",
+    borderRadius: "2em",
+    bg: "blue.300",
+    px: { base: "1.5em", md: "2em" },
+    gap: { base: "0.5em", md: "1em" },
+    fontSize: { base: "sm", md: "lg" },
+    mt: "2em",
+  };
+
+  return (
+    <>
+      <Textarea
+        bg="gray.100"
+        color="gray.800"
+        height={150}
+        value={input}
+        onChange={(e: {
+          target: {
+            value: any;
+          };
+        }) => setInput(e.target.value)}
+      ></Textarea>
+      <MotionButton {...submitButtonStyles} onClick={submitText}>
+        🪄 Extract Keywords
+      </MotionButton>
+    </>
+  );
+}
 
 export default TextInput;
