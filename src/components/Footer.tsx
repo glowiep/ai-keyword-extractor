696b279db1f1ebfc8eeae0f9bc6f1e13c7b2d804
Gloria Lim <glorialimartt@gmail.com>
2024-05-22 14:17:13 +0800

    Remove unused package

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -17,6 +17,18 @@ function Footer() {
         </Box>
         <Text>{applicationText.Footer.credits}</Text>
       </Flex>
+      <Flex justifyContent="center" alignItems="center">
+        <Box paddingRight="0.5em">
+          <Image
+            bg="white"
+            boxSize="1.5em"
+            borderRadius="full"
+            padding="0.2em"
+            src="https://raw.githubusercontent.com/glowiep/ai-keyword-extractor/486b9b41f1f5eaa003d35761134757f1cb34d1b6/src/assets/openai-logo.svg"
+            alt="Github"
+          />
+        </Box>
+      </Flex>
     </Box>
   );
 }
